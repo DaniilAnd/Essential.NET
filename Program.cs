b292ab1de669a7b6d81b1a78063b252bd5f70097
Storonkin <lisichkindan@yandex.ru>
2020-01-17 23:51:03 +0300

    1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace Essential.NET
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Вывод в консоль");
+            // основные типы данных int double bool string char decimal
+            // циклы for foreach while   do while case
+            // массивы [] list ienumerable dictionary hashset
+            //  структуры данных стек очередь дерево би-дерево графы хеш-таблицы
+            // лог операции
+            // ООП (классы, асбтракция, наследование)
+            
+
+
+
         }
     }
 }
